6e1fb768c56ac17aa6d7671a7a143a59f584f9d0
Gerardo Cordero <cordero.gerard@gmail.com>
2018-01-15 15:11:07 -0400

    Create Mongoose connection Routes and Models.
    Next: Test Todos API.

---
+++ server/server.js
@@ -0,0 +1,35 @@
+// Third party modules
+
+let express = require('express');
+let bodyParser = require('body-parser');
+
+// My modules exported
+let {mongoose} = require('./db/mongoose');
+let {Todo} = require('./models/todo');
+let {User} = require('./models/user');
+
+
+// Express REST API Server Instance
+let app = express();
+
+// Use Middleware to allow express read and send json data, body parser wraps express.
+app.use(bodyParser.json());
+
+app.post('/todos',(req, res)=>{
+    let todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc)=>{
+        res.send(doc);
+    }, (e)=>{
+        res.status(400).send(e);
+    })
+
+});
+
+app.listen(3000, ()=>{
+    console.log('Started on port 3000');
+});
+
+module.exports = {app}
